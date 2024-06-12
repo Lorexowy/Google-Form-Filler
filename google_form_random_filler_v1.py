0cf4ff60a40b0af2f7d2de0981f7703fe18377fe
Lorexowy <mateusz.michel7@gmail.com>
2024-06-12 12:33:01 +0200

    push v1.1.1

--- google_form_random_filler_v1.py
+++ google_form_random_filler_v1.py
@@ -9,30 +9,31 @@ import random
 import webbrowser
 import logging
 
-# Konfiguracja logowania do pliku
+# Logging configuration function
 def setup_logging(enable_logging):
     if enable_logging:
         logging.basicConfig(filename='form_filler.log', level=logging.INFO, format='%(asctime)s - %(message)s')
     else:
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
+# Function to fill the form
 def fill_form(driver, form_url, text_inputs):
     driver.get(form_url)
-    time.sleep(5)  # Czekaj
+    time.sleep(5)  # Wait
 
     filled_first_text_input = False
 
     while True:
         try:
             if not filled_first_text_input:
-                # Tekst - tylko pierwsze pole
+                # Text input - only the first field
                 text_input_element = driver.find_element(By.XPATH, '//input[@type="text"]')
                 if text_inputs:
                     input_text = random.choice(text_inputs)
                     text_input_element.send_keys(input_text)
                     logging.info(f"Entered text: {input_text}")
                 filled_first_text_input = True
-                time.sleep(0.1)  # Opóźnienie
+                time.sleep(0.1)  # Delay
 
             # Radiobuttons
             radio_groups = driver.find_elements(By.XPATH, '//div[@role="radiogroup"]')
@@ -40,76 +41,78 @@ def fill_form(driver, form_url, text_inputs):
                 options = radio_group.find_elements(By.XPATH, './/div[@role="radio"]')
                 if options:
                     random.choice(options).click()
-                    time.sleep(0.1)  # Opóźnienie
+                    time.sleep(0.1)  # Delay
 
-            # Checkboxy
+            # Checkboxes
             checkbox_groups = driver.find_elements(By.XPATH, '//div[contains(@class, "eBFwI") and not(contains(@class, "RVLOe"))]')
             for checkbox_group in checkbox_groups:
                 checkboxes = checkbox_group.find_elements(By.XPATH, './/div[@role="checkbox"]')
                 for checkbox in checkboxes:
                     label_element = checkbox.find_element(By.XPATH, '..')
                     label = label_element.text.strip()
-                    if label and random.choice([True, False]):  # Losowy
+                    if label and random.choice([True, False]):  # Random
                         checkbox.click()
-                        time.sleep(0.1)  # Opóźnienie
+                        time.sleep(0.1)  # Delay
                         logging.info(f"Clicked checkbox: {label}")
 
-            # Skala
+            # Scale
             scale_questions = driver.find_elements(By.XPATH, '//div[@role="radiogroup"]//div[@data-value]')
             for scale_question in scale_questions:
                 options = scale_question.find_elements(By.XPATH, './/div[@role="radio"]')
                 if options:
                     random.choice(options).click()
-                    time.sleep(0.1)  # Opóźnienie
+                    time.sleep(0.1)  # Delay
                     logging.info(f"Clicked scale option: {options[random.randint(0, len(options) - 1)].get_attribute('aria-label')}")
 
-            # Rozwijana lista
+            # Dropdown
             dropdowns = driver.find_elements(By.XPATH, '//div[@role="listbox"]')
             for dropdown in dropdowns:
                 dropdown.click()
-                time.sleep(0.1)  # Opóźnienie
+                time.sleep(0.1)  # Delay
                 options = dropdown.find_elements(By.XPATH, './/div[@role="option"]')
                 if options:
                     random.choice(options).click()
-                    time.sleep(0.1)  # Opóźnienie
+                    time.sleep(0.1)  # Delay
                     logging.info(f"Selected dropdown option: {options[random.randint(0, len(options) - 1)].text}")
 
-            # Dalej
-            next_button = driver.find_elements(By.XPATH, '//span[contains(text(), "Dalej") or contains(text(), "Next")]')
+            # Next
+            next_button = driver.find_elements(By.XPATH, '//span[contains(text(), "Next")]')
             if next_button:
                 next_button[0].click()
-                time.sleep(0.5)  # Czekaj
+                time.sleep(0.5)  # Wait
             else:
-                break  # Koniec
+                break  # End
 
         except Exception as e:
             logging.error(f"An error occurred: {e}")
             break
 
     try:
-        # Prześlij
-        submit_button = driver.find_element(By.XPATH, '//span[contains(text(), "Prześlij") or contains(text(), "Submit")]')
+        # Submit
+        submit_button = driver.find_element(By.XPATH, '//span[contains(text(), "Submit")]')
         submit_button.click()
-        time.sleep(0.5)  # Czekaj
+        time.sleep(0.5)  # Wait
     except Exception as e:
         logging.error(f"An error occurred while submitting the form: {e}")
 
+# Main function
 def main(form_url, repetitions, text_inputs, enable_logging):
     setup_logging(enable_logging)
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
     try:
-        for _ in range(repetitions):  # Powtórzenia
+        for _ in range(repetitions):  # Repetitions
             fill_form(driver, form_url, text_inputs)
-            time.sleep(0.5)  # Czekaj
+            time.sleep(0.5)  # Wait
     finally:
         driver.quit()
 
-    # Wyświetl okno dialogowe z informacją o zakończeniu pracy
+    # Show completion message
     root = tk.Tk()
-    root.withdraw()  # Ukryj główne okno
+    root.withdraw()  # Hide the main window
     messagebox.showinfo("Completion", f"Surveys have been filled {repetitions} times. Thank you! :-)")
 
+# GUI start function
 def start_gui():
     def on_submit(url_entry, repetitions_entry, text_inputs_entry, log_var):
         form_url = url_entry.get()
@@ -141,38 +144,38 @@ def start_gui():
 
     root = tk.Tk()
     root.title("Google Random Form Filler")
-    root.configure(bg='#ADD8E6')  # Niebieskie tło
+    root.configure(bg='#ADD8E6')  # Blue background
 
-    # Tytuł
+    # Title
     title_label = tk.Label(root, text="Google Random Form Filler", font=("Helvetica", 16), bg='#ADD8E6')
     title_label.grid(row=0, column=0, columnspan=2, pady=10)
 
-    # Etykieta i pole do wprowadzania URL
+    # Label and entry for URL
     tk.Label(root, text="Google Form URL:", bg='#ADD8E6').grid(row=1, column=0, padx=10, pady=10, sticky='e')
     url_entry = tk.Entry(root, width=50)
     url_entry.grid(row=1, column=1, padx=10, pady=10)
 
-    # Etykieta i pole do wprowadzania liczby powtórzeń
+    # Label and entry for number of repetitions
     tk.Label(root, text="Number of repetitions:", bg='#ADD8E6').grid(row=2, column=0, padx=10, pady=10, sticky='e')
     repetitions_entry = tk.Entry(root, width=10)
     repetitions_entry.grid(row=2, column=1, padx=10, pady=10, sticky='w')
 
-    # Etykieta i pole do wprowadzania tekstu do pól tekstowych z przykładem
+    # Label and entry for text inputs with example
     tk.Label(root, text="Text inputs (comma separated):", bg='#ADD8E6').grid(row=3, column=0, padx=10, pady=10, sticky='e')
     text_inputs_entry = tk.Entry(root, width=50)
     text_inputs_entry.grid(row=3, column=1, padx=10, pady=10)
     tk.Label(root, text="Example: Poland, Ireland, Germany", bg='#ADD8E6').grid(row=4, column=1, padx=10, pady=10, sticky='w')
 
-    # Opcja zapisywania logów
+    # Logging option
     log_var = tk.IntVar()
     log_check = tk.Checkbutton(root, text="Enable logging", variable=log_var, bg='#ADD8E6')
     log_check.grid(row=5, column=0, columnspan=2)
 
-    # Przycisk Submit
+    # Submit button
     submit_button = tk.Button(root, text="Submit", command=lambda: on_submit(url_entry, repetitions_entry, text_inputs_entry, log_var), bg='#87CEEB')
     submit_button.grid(row=6, column=0, columnspan=2, pady=20)
 
-    # Napis na dole
+    # Footer text
     footer = tk.Label(root, text="created and coded by www.mateuszmichel.com", fg="blue", cursor="hand2", bg='#ADD8E6')
     footer.grid(row=7, column=0, columnspan=2, pady=10)
     footer.bind("<Button-1>", open_url)
